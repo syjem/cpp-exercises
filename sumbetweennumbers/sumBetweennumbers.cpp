a8c81a54487a1a579b95869caf0eb51997f1cd41
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-02-14 16:16:04 +0800

    sum between 1 to n

---
+++ sumbetweennumbers/sumBetweennumbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n, sum = 0;
+
+    cin >> n;
+    
+    for(int i = 1; i<=n; i++) {
+        sum += i;
+    }
+
+        cout <<"The sum of the numbers between 1 to "<< n <<" is " << sum <<"." <<endl;
+
+    return 0;
+}
